70f5fcbe7781f1ee9ce04fafb3857efe58260bcb
Matt Rubin <mattrubin@me.com>
2012-05-07 11:34:30 -0700

    Added tests for layout links and full_title application helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,4 +32,17 @@ describe "Static pages" do
     it { should have_selector 'h1',    text: 'Contact' }
     it { should have_selector 'title', text: full_title('Contact') }
   end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title
+    page.should_not have_selector 'title', text: '| Home'
+  end
 end
