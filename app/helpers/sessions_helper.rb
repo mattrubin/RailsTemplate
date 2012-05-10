35aa44859a36bc4214f79ed1b27a8034fa1f977d
Matt Rubin <mattrubin@me.com>
2012-05-10 00:47:32 -0700

    Remember Me option on sign-in (closes #1)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,10 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    if params[:remember_me]
+      cookies.permanent[:remember_token] = user.remember_token
+    else
+      cookies[:remember_token] = user.remember_token
+    end
     current_user = user
   end
   
