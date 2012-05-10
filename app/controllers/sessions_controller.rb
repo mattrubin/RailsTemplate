2de38f301f1d4bcef221c1f168119bc4d956f1d8
Matt Rubin <mattrubin@me.com>
2012-05-09 22:28:16 -0700

    Sign in form and failure message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
   
   def destroy
