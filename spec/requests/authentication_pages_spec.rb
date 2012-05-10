d6d63239710681e497e1eef2cfe6acc215aadd93
Matt Rubin <mattrubin@me.com>
2012-05-10 01:13:35 -0700

    [minor] Test to ensure sign-in email is case-insensitive

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -37,7 +37,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email
+        fill_in "Email",    with: user.email.upcase # ensures case-insensitivity
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
