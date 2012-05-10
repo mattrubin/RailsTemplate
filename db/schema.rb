163eb3bb23a27daac121981bbb54ef387281c429
Matt Rubin <mattrubin@me.com>
2012-05-10 00:13:57 -0700

    Ensures that remember_token is unique (closes #5)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120510053140) do
+ActiveRecord::Schema.define(:version => 20120510065812) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,6 @@ ActiveRecord::Schema.define(:version => 20120510053140) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token", :unique => true
 
 end
