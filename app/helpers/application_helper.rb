6ef4c1cea0a45e18ce6ad465d0a672f48a4942fd
Matt Rubin <mattrubin@me.com>
2012-05-10 12:46:45 -0700

    [refactor] Spec utility functions to decouple test logic from implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title='')
     base_title = "Rails Template"
-    if page_title.empty?
+    if page_title.nil? || page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,49 +7,43 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
     
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_heading 'Sign in' }
+    it { should have_title   'Sign in' }
   end
   
   describe "signin" do
     before { visit signin_path }
     
     describe "with invalid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",        with: user.email
-        check "Remember Me"
-        click_button "Sign in"
-      end
-      before { click_button "Sign in" }
+      create_user
+      before { invalid_signin(user) }
       
-      it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-      it { should have_field('Email', :with => user.email)}
-      specify { find_field('Remember Me').should be_checked}
+      it { should have_title 'Sign in' }
+      it { should have_error_message 'Invalid' }
+      it { should have_field('Email', :with => user.email) }
+      specify { find_field('Remember Me').should be_checked }
       
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message }
       end
     end
     
     describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
+      create_user
       before do
-        fill_in "Email",    with: user.email.upcase # ensures case-insensitivity
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        user.email = user.email.upcase # ensures case-insensitivity
+        valid_signin(user)
       end
       
-      it { should have_selector('title', text: user.name) }
+      it { should have_title user.name }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
       describe "followed by signout" do
         before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        it { should have_link 'Sign in' }
       end
     end
   end
