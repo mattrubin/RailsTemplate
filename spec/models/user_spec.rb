61ff4556575c7d19d05e36ef55944bd1e6ba74b6
Matt Rubin <mattrubin@me.com>
2012-06-04 15:26:24 -0700

    Preliminary feed display

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -142,5 +142,15 @@ describe User do
         Post.find_by_id(post.id).should be_nil
       end
     end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:post, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_post) }
+      its(:feed) { should include(older_post) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
   end
 end
