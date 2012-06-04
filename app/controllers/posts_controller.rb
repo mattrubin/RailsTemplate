50fd050e94af0e343973ed2448f273bb81537b96
Matt Rubin <mattrubin@me.com>
2012-06-04 13:37:28 -0700

    Post creation and input form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
+    @post = current_user.posts.build(params[:post])
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
