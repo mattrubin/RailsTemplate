c5b8c4a992115d88ff8d7c0a58c8b4b1891c072a
Matt Rubin <mattrubin@me.com>
2012-06-04 12:38:44 -0700

    Posts listed on User#show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,10 +52,19 @@ describe "User pages" do
 
   describe "profile page" do
     create_user
+    let!(:p1) { FactoryGirl.create(:post, user: user, content: "Foo") }
+    let!(:p2) { FactoryGirl.create(:post, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_heading user.name }
     it { should have_title   user.name }
+
+    describe "posts" do
+      it { should have_content(p1.content) }
+      it { should have_content(p2.content) }
+      it { should have_content(user.posts.count) }
+    end
   end
 
   describe "signup page" do
