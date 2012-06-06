8e5af391d875cc130f2264f496bc8a297a817528
Matt Rubin <mattrubin@me.com>
2012-06-05 23:18:39 -0700

    Added a character countdown to the post entry form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
+  MAX_LENGTH = 140
   attr_accessible :content
   belongs_to :user
 
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: MAX_LENGTH }
   validates :user_id, presence: true
 
   default_scope order: 'posts.created_at DESC'
